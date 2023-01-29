486eeaf8b69700cf155d952ea898bcfef1e04232
Christina <bihtya@mail.ru>
2023-01-29 20:13:54 +0300

    Вторая задача

---
+++ Exp02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int ValueInput()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void SumElementNumber(int Number)
+{
+    int sum = 0;
+    while (Number > 0)
+    {
+        sum += Number % 10;
+        Number /= 10;
+    }
+    Console.WriteLine(sum);
+}
+int number = ValueInput();
+SumElementNumber(number);
